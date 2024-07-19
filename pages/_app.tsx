95f2c8e7dcf7a3353b3061a6567e95ce7be14ecc
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2024-07-19 16:23:46 +0530

    Project setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,16 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <>
+      <div className="vertical-lines">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
