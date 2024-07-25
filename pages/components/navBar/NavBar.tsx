bb7ef54ff7804919cdb2226d760b740548ddb326
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2024-07-26 04:54:35 +0530

    UI updated for nav bar with hamburger

--- pages/components/navBar/NavBar.tsx
+++ pages/components/navBar/NavBar.tsx
@@ -1,14 +1,31 @@
 import Link from 'next/link';
 import styles from './NavBar.module.css'
+import { useState } from 'react';
 
 const NavBar = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+
   return (
     <nav className={styles.nav}>
-      <div className={styles.logo}>  <Link href="/">SPARKBIT</Link></div>
-      <div className={styles.menu}>
+ 
+      <div className={styles.logo}>
+        <Link href="/">SPARKBIT</Link>
+      </div>
+      <div className={`${styles.menu} ${isMenuOpen ? styles.showMenu : ''}`}>
         <Link href="/caseStudies">CASE STUDIES</Link>
         <Link href="/services">SERVICES</Link>
         <Link href="/meetus">MEET US</Link>
+      </div>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </nav>
   );
