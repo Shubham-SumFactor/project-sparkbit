0a228356ec3a52b1a02e7f08e0185a9c18934936
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2024-08-02 01:35:20 +0530

    home page responsiveness issues fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -187,11 +187,11 @@ export default function Home() {
             <p>The full ML project life-cycle, from dataset building to model architecture definition and training to deployment (both in the cloud and on edge). Techniques from areas such as CV and NLP and their applications to various business problems.</p>
           </div>
           <div className={styles.serviceCard}>
-            <h2>Architecture &amp; Backend</h2>
+            <h2>Architecture & Backend</h2>
             <p>Systems for data analysis and processing, including high-frequency data, time-series or IoT data. Focusing on reliability and scalability of solutions while maintaining low operating costs.</p>
           </div>
-          <div className={styles.serviceCard}>
-            <h2>R&amp;D <br></br>Prototypes</h2>
+          <div className={styles.serviceCard2}>
+            <h2>R&D Prototypes</h2>
             <p>Unique problems requiring the development of new techniques or algorithms and excellent programming skills. Some of these problems will prove unsolvable, others will lead to the best innovations.</p>
           </div>
 
