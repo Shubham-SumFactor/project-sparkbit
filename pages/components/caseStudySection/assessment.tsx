fe49d058e44ce519d6d997cd4c7debbd7b178ea6
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2024-08-06 14:04:52 +0530

    case study section modal UI and functionality updated

--- pages/components/caseStudySection/assessment.tsx
+++ pages/components/caseStudySection/assessment.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './assessment.module.css';
+import Image from 'next/image';
+
 
 interface AutomatedAssessmentProps {
   sideHeading: string;
@@ -10,14 +12,19 @@ interface AutomatedAssessmentProps {
 }
 
 
-const AutomatedAssessment : React.FC<AutomatedAssessmentProps> = ({ sideHeading ,title, description, readMoreLink, graphImage }) => {
+const AutomatedAssessment: React.FC<AutomatedAssessmentProps> = ({ sideHeading, title, description, readMoreLink, graphImage }) => {
   return (
     <div className={styles.container}>
       <div className={styles.category}>{sideHeading}</div>
       <div className={styles.content}>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.description}>{description}</p>
-        <a href={readMoreLink} className={styles.readMore}>Read this case →</a>
+        {/* <a href={readMoreLink} className={styles.readMore}>Read this case →</a> */}
+        <button className={styles.readThisButton} onClick={() => window.location.href = readMoreLink}>
+          <span>Read this case</span>
+          <Image src="/caseStudyArrow.svg" alt="rightarrow" height={15} width={18} className={styles.readThisButtonArrow} />
+        </button>
+
       </div>
       <div className={styles.graph}>
         <img src={graphImage} alt="Graph" />
