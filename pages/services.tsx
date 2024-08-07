912e0230d9efcf75cb678d0a25038a2b26a612d7
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2024-08-07 15:19:39 +0530

    Services page and css updated

--- pages/services.tsx
+++ pages/services.tsx
@@ -5,6 +5,9 @@ import NavBar from './components/navBar/NavBar';
 import ServicesCard from './components/servicesCard/servicesCard';
 import MLProcessSection from './components/mlCard/MLProcessSection';
 import ProjectYear from './components/projectYear/projectYear';
+import TechnologyCard from './components/technologyCard/technologyCard';
+import TeamAddOns from './components/teamAddOns/teamAddOns';
+import ContactUs from './components/contactUs/ContactUs';
 
 const Services = () => {
 
@@ -29,75 +32,15 @@ const Services = () => {
                     <div className={styles.graphicElement}></div>
                 </div>
 
-
-                <section className={styles.about}>
-                    <h2>ABOUT</h2>
-                    <div className={styles.content}>
-                        <p>
-                            These are the three main areas in which we operate, although a
-                            challenging project may be anything our client finds innovative and
-                            difficult. If this is your case, send us a message.
-                        </p>
-                        <p>Let's chat about it.</p>
-
-                        <div className={styles.areas}>
-                            <div className={styles.area}>
-                                <h3>Machine Learning</h3>
-                                <button className={styles.scrollCircle} >
-                                    <img src="/leftArrowImage.svg" alt="downArrow" height={18} width={18} className={styles.scrollButton} />
-                                </button>
-                            </div>
-                            <div className={styles.area}>
-                                <h3>Architecture & Backend</h3>
-                                <button className={styles.scrollCircle} >
-                                    <img src="/leftArrowImage.svg" alt="downArrow" height={18} width={18} className={styles.scrollButton} />
-                                </button>
-                            </div>
-                            <div className={styles.area}>
-                                <h3>R&D Prototypes</h3>
-                                <button className={styles.scrollCircle} >
-                                    <img src="/leftArrowImage.svg" alt="downArrow" height={18} width={18} className={styles.scrollButton} />
-                                </button>
-                            </div>
-
-                        </div>
-                    </div>
-                </section>
-
-                <section className={styles.threeCircles}>
-                    <img className={styles.servicesImage} src="/3circles.svg" alt="whatwedo" />
-                </section>
-
-                <section className={styles.machineLearning}>
-                    <h2>
-                        Machine Learning
-                    </h2>
-                    <p>
-                        We build robust and scalable Machine Learning solutions. We automate data processing and develop models and algorithms that become our clients' core IP. Knowing that the ML project can be complex and unpredictable, we lead you through it with a fully-managed process.
-                    </p>
-                </section>
-
                 <ServicesCard />
 
-                <section className={styles.workflow} >
-                    <h2>OUR ML WORKFLOW</h2>
-                    <div>
-                        <p>
-                            Machine learning projects are all about experimentation, rapidly testing various ideas, some of which are successful and others leading nowhere. We run hundreds of experiments with different hyperparameters, model variants, and training sets. After several months we need to identify which experiment performed best and which model variant was used there.
-                        </p>
+                <TechnologyCard />
 
-                        <p style={{ paddingTop: '10px' }}>
-                            Usually, things get even more complex, as systems often use not one but a chain of linked models. We can control it only when following a well-defined, strict process supported by proper tooling - our ML Ops framework.
-                        </p>
-                    </div>
-                </section>
+                <TeamAddOns />
 
-                <MLProcessSection />
-
-                <ProjectYear />
             </main>
 
-            {/* <ContactUs /> */}
+            <ContactUs />
 
 
         </div>
