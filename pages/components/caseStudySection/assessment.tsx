c79b743aa641e4fec8b8671f7452719a1a278925
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2024-08-08 10:27:50 +0530

    Read this case for the assesment / Project updated

--- pages/components/caseStudySection/assessment.tsx
+++ pages/components/caseStudySection/assessment.tsx
@@ -20,10 +20,10 @@ const AutomatedAssessment: React.FC<AutomatedAssessmentProps> = ({ sideHeading, 
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.description}>{description}</p>
         {/* <a href={readMoreLink} className={styles.readMore}>Read this case →</a> */}
-        <button className={styles.readThisButton} onClick={() => window.location.href = readMoreLink}>
+        {/* <button className={styles.readThisButton} onClick={() => window.location.href = readMoreLink}>
           <span>Read this case</span>
           <Image src="/caseStudyArrow.svg" alt="rightarrow" height={15} width={18} className={styles.readThisButtonArrow} />
-        </button>
+        </button> */}
 
       </div>
       <div className={styles.graph}>
