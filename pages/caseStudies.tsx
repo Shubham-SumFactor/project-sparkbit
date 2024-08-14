c8a7ce04c465225e547546177f0a53d0d1c77c07
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2024-08-14 12:40:50 +0530

    image added for projects , svt

--- pages/caseStudies.tsx
+++ pages/caseStudies.tsx
@@ -53,7 +53,7 @@ const CaseStudies = () => {
                     title="Web & Mobile Solutions"
                     description="Develop web & mobile solutions to replace outdated systems with enhance functionality."
                     readMoreLink="/caseStudies"
-                    graphImage="/sumfactor/svtProject.jpeg"
+                    graphImage="/sumfactor/svt-inner-portfolio.png"
                 />
 
                 <AutomatedAssessment
